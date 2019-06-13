77e754ebc9c43d58fb89fb8f44ce0f76bed9fdce
lcnssamlcn <lcnssamlcn@gmail.com>
2019-06-13 21:08:21 +0800

    implemented equation display

--- app/src/main/java/com/example/lcn/calc/MainActivity.java
+++ app/src/main/java/com/example/lcn/calc/MainActivity.java
@@ -3,9 +3,40 @@ package com.example.lcn.calc;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Html;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
+import java.util.List;
+
+/**
+ * simple calculator app
+ * @author lcn
+ */
 public class MainActivity extends AppCompatActivity {
+    public static final String TAG = "MainActivity";
+    /**
+     * cursor in the equation display
+     */
+    private static final String CURSOR = "\u258A";
+
+    /**
+     * equation display
+     */
+    TextView eqt;
+    /**
+     * calculation result display
+     */
+    TextView result;
+
+    /**
+     * @return the current cursor pos in equation display
+     */
+    public int getCursorPos() {
+        return this.eqt.getText().toString().indexOf(MainActivity.CURSOR);
+    }
+
     /**
      * initialize special math symbols
      */
@@ -14,10 +45,194 @@ public class MainActivity extends AppCompatActivity {
         btnExp.setText(Html.fromHtml(getResources().getString(R.string.btn_exp)));
     }
 
+    /**
+     * insert a new symbol to the equation display.
+     * @param symbol symbol to insert
+     */
+    public void insertSymbol(String symbol) {
+        String eqtStr = MainActivity.this.eqt.getText().toString();
+        int cursorPos = MainActivity.this.getCursorPos();
+        if (cursorPos == -1) {
+            resetDisplay();
+            return;
+        }
+        StringBuilder sb = new StringBuilder();
+        if (cursorPos > 0)
+            sb.append(eqtStr.substring(0, cursorPos));
+        sb.append(symbol);
+        sb.append(MainActivity.CURSOR);
+        if (cursorPos + 1 < eqtStr.length())
+            sb.append(eqtStr.substring(cursorPos + 1));
+        MainActivity.this.eqt.setText(sb.toString());
+    }
+
+    /**
+     * @return the previous symbol in the equation display.
+     */
+    public Symbol getPrevSymbol() {
+        List<Symbol> sortedSymbols = Symbol.sort();
+        String eqtStr = MainActivity.this.eqt.getText().toString();
+        int cursorPos = MainActivity.this.getCursorPos();
+        if (cursorPos == -1) {
+            resetDisplay();
+            return null;
+        }
+        for (Symbol sym : sortedSymbols) {
+            String symRepr = sym.getRepr();
+            if (cursorPos - symRepr.length() < 0)
+                continue;
+            if (eqtStr.substring(cursorPos - symRepr.length(), cursorPos).equals(symRepr))
+                return sym;
+        }
+        return null;
+    }
+
+    /**
+     * @return the next symbol in the equation display.
+     */
+    public Symbol getNextSymbol() {
+        List<Symbol> sortedSymbols = Symbol.sort();
+        // Log.i(MainActivity.TAG, "sorted: " + sortedSymbols.toString());
+        String eqtStr = MainActivity.this.eqt.getText().toString();
+        int cursorPos = MainActivity.this.getCursorPos();
+        if (cursorPos == -1) {
+            resetDisplay();
+            return null;
+        }
+        for (Symbol sym : sortedSymbols) {
+            String symRepr = sym.getRepr();
+            int endSym = cursorPos + symRepr.length() + 1;
+            if (endSym == eqtStr.length()) {
+                if (eqtStr.substring(cursorPos + 1).equals(symRepr))
+                    return sym;
+            }
+            else if (endSym < eqtStr.length()) {
+                if (eqtStr.substring(cursorPos + 1, endSym).equals(symRepr))
+                    return sym;
+            }
+        }
+        return null;
+    }
+
+    /**
+     * executed when a digit symbol such as "1", "2", "3" and etc. is clicked.
+     * @param v clicked button. (It is castable to <i>Button</i>.)
+     */
+    public void clickNum(View v) {
+        Symbol sym = Symbol.findSymbolByID(v.getId());
+        if (sym == null)
+            return;
+        insertSymbol(sym.getRepr());
+    }
+
+    /**
+     * executed when an operator symbol such as "(", ")", "+" and etc. is clicked.
+     * @param v clicked button. (It is castable to <i>Button</i>.)
+     */
+    public void clickOp(View v) {
+        Symbol sym = Symbol.findSymbolByID(v.getId());
+        if (sym == null)
+            return;
+        insertSymbol(sym.getRepr());
+    }
+
+    /**
+     * initialize general buttons, such as "AC", left navigation button and right navigation button.
+     */
+    private void initBtnGeneral() {
+        Button btnAC = (Button) findViewById(R.id.btn_AC);
+        btnAC.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                resetDisplay();
+            }
+        });
+        /* nav */
+        Button btnLeft = (Button) findViewById(R.id.btn_left);
+        btnLeft.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String eqtStr = MainActivity.this.eqt.getText().toString();
+                if (eqtStr.charAt(0) == MainActivity.CURSOR.charAt(0))
+                    return;
+
+                int cursorPos = getCursorPos();
+                if (cursorPos == -1) {
+                    resetDisplay();
+                    return;
+                }
+                Symbol sym = getPrevSymbol();
+                // internal error
+                if (sym == null) {
+                    resetDisplay();
+                    return;
+                }
+                Log.i(MainActivity.TAG, String.format("prev symbol: \"%s\"", sym.getRepr()));
+                StringBuilder sb = new StringBuilder();
+                if (cursorPos - sym.getRepr().length() > 0)
+                    sb.append(eqtStr.substring(0, cursorPos - sym.getRepr().length()));
+                sb.append(MainActivity.CURSOR);
+                sb.append(sym.getRepr());
+                if (cursorPos + 1 < eqtStr.length())
+                    sb.append(eqtStr.substring(cursorPos + 1));
+                MainActivity.this.eqt.setText(sb.toString());
+            }
+        });
+        Button btnRight = (Button) findViewById(R.id.btn_right);
+        btnRight.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String eqtStr = MainActivity.this.eqt.getText().toString();
+                if (eqtStr.charAt(eqtStr.length() - 1) == MainActivity.CURSOR.charAt(0))
+                    return;
+
+                int cursorPos = getCursorPos();
+                if (cursorPos == -1) {
+                    resetDisplay();
+                    return;
+                }
+                Symbol sym = getNextSymbol();
+                // internal error
+                if (sym == null) {
+                    resetDisplay();
+                    return;
+                }
+                Log.i(MainActivity.TAG, String.format("next symbol: \"%s\"", sym.getRepr()));
+                StringBuilder sb = new StringBuilder();
+                if (cursorPos > 0)
+                    sb.append(eqtStr.substring(0, cursorPos));
+                sb.append(sym.getRepr());
+                sb.append(MainActivity.CURSOR);
+                if (cursorPos + sym.getRepr().length() + 1 < eqtStr.length())
+                    sb.append(eqtStr.substring(cursorPos + sym.getRepr().length() + 1));
+                MainActivity.this.eqt.setText(sb.toString());
+            }
+        });
+    }
+
+    /**
+     * initialize the calculator display
+     */
+    private void initDisplay() {
+        this.eqt = (TextView) findViewById(R.id.eqt);
+        this.result = (TextView) findViewById(R.id.result);
+        resetDisplay();
+    }
+
+    /**
+     * reset the calculator display
+     */
+    private void resetDisplay() {
+        this.eqt.setText(MainActivity.CURSOR);
+        this.result.setText("0");
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initBtnText();
+        initDisplay();
+        initBtnGeneral();
     }
 }
---
+++ app/src/main/java/com/example/lcn/calc/Symbol.java
@@ -0,0 +1,150 @@
+package com.example.lcn.calc;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+/**
+ * all symbols that are supported in this calculator
+ * @author lcn
+ */
+public enum Symbol {
+    /**
+     * digit 1
+     */
+    SYM_1("1", R.id.btn_1),
+    /**
+     * digit 2
+     */
+    SYM_2("2", R.id.btn_2),
+    /**
+     * digit 3
+     */
+    SYM_3("3", R.id.btn_3),
+    /**
+     * digit 4
+     */
+    SYM_4("4", R.id.btn_4),
+    /**
+     * digit 5
+     */
+    SYM_5("5", R.id.btn_5),
+    /**
+     * digit 6
+     */
+    SYM_6("6", R.id.btn_6),
+    /**
+     * digit 7
+     */
+    SYM_7("7", R.id.btn_7),
+    /**
+     * digit 8
+     */
+    SYM_8("8", R.id.btn_8),
+    /**
+     * digit 9
+     */
+    SYM_9("9", R.id.btn_9),
+    /**
+     * digit 0
+     */
+    SYM_0("0", R.id.btn_0),
+    /**
+     * left parenthesis "("
+     */
+    SYM_LEFT_PAREN("(", R.id.btn_left_paren),
+    /**
+     * right parenthesis ")"
+     */
+    SYM_RIGHT_PAREN(")", R.id.btn_right_paren),
+    /**
+     * multiplication operator
+     */
+    SYM_MUL("\u00D7", R.id.btn_mul),
+    /**
+     * division operator
+     */
+    SYM_DIV("/", R.id.btn_div),
+    /**
+     * subtraction operator
+     */
+    SYM_MINUS("-", R.id.btn_minus),
+    /**
+     * exponent operator
+     */
+    SYM_EXP("^(", R.id.btn_exp),
+    /**
+     * decimal operator
+     */
+    SYM_DOT(".", R.id.btn_dot),
+    /**
+     * plus operator
+     */
+    SYM_PLUS("+", R.id.btn_plus),
+    /**
+     * reference of the previous calculation result
+     */
+    SYM_ANS("Ans", R.id.btn_ans);
+
+    /**
+     * symbol representation in the {@link MainActivity#eqt equation display}.
+     */
+    private String repr;
+    /**
+     * symbol button id
+     */
+    private int id;
+
+    /**
+     * create a new math symbol
+     * @param repr representation in the {@link MainActivity#eqt equation display}.
+     * @param id symbol button id
+     */
+    Symbol(String repr, int id) {
+        this.repr = repr;
+        this.id = id;
+    }
+
+    /**
+     * @return symbol representation in the {@link MainActivity#eqt equation display}.
+     */
+    public String getRepr() {
+        return this.repr;
+    }
+
+    /**
+     * @return symbol button id
+     */
+    public int getID() {
+        return this.id;
+    }
+
+    /**
+     * find symbol by button ID
+     * @param id calculator button id
+     * @return the corresponding symbol if found; otherwise null.
+     */
+    public static Symbol findSymbolByID(int id) {
+        for (Symbol sym: Symbol.values()) {
+            if (sym.getID() == id)
+                return sym;
+        }
+        return null;
+    }
+
+    /**
+     * sort all defined math symbols by the length of their string representation in descending order.
+     * @return sorted symbols
+     */
+    public static List<Symbol> sort() {
+        List<Symbol> sortedSymbols = Arrays.asList(Symbol.values());
+        Collections.sort(sortedSymbols, new Comparator<Symbol>() {
+            @Override
+            public int compare(Symbol a, Symbol b) {
+                return b.getRepr().length() - a.getRepr().length();
+            }
+        });
+        return sortedSymbols;
+    }
+}
